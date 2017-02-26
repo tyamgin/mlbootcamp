6c75a912e3bd202b7901a7a51902d66ba9460222
tyamgin <tyamgin@gmail.com>
2017-02-26 23:35:57 +0300

    lgb+svm

--- championship10/genetic.R
+++ championship10/genetic.R
@@ -18,6 +18,10 @@ mutation = function(a, p = 0.1) {
   a
 }
 
+vapnik.logloss = function(nrows, ncols) { 
+  (ncols * (log(2*nrows/ncols) + 1) / nrows - log(0.05) / nrows)^0.5 / 200
+}
+
 tqfoldEstimation = function(XL, G, teach) {
   p = sum(G)
   if (p <= 1)
@@ -26,36 +30,39 @@ tqfoldEstimation = function(XL, G, teach) {
   L = nrow(XL)
   m = ncol(XL) - 1
   subXL = XL[, c(which(G == 1), m + 1)]
-  mean(validation.tqfold(subXL, teach, folds=5, iters=2, verbose=F))
+  
+  e = mean(validation.tqfold(subXL, teach, folds=5, iters=2, verbose=F))
+  list(int=e, ext=(e + vapnik.logloss(L, p)))
 }
 
 geneticSelect = function(iterations,  # количество итераций
                          XL, # выборка
                          teach, # teach(XL) - обучение
-                         maxPopulationSize, # размер попул¤ции
+                         maxPopulationSize, # размер популяции
                          mutationProb = 0.1, # вероятность мутации
                          estimate = tqfoldEstimation, # оценка
-                         d = 100 # дополнительный параметр останова
+                         d = 100, # дополнительный параметр останова
+                         startOnesProbab = 0.5,
+                         removeDuplicates=T
 ) {
   L = nrow(XL)
   size = ncol(XL) - 1
-  R = matrix(NA, maxPopulationSize, size + 1) # генерация хромосом
+  R = matrix(NA, maxPopulationSize, size + 2) # генерация хромосом
   
   chr = function(vec) { # получить хромосому
-    vec[-length(vec)]
+    vec[1:size]
   }
-  Q = function(vec) { # получить её оценку
-    n = length(vec)
-    if (is.na(vec[n]))
-      vec[n] = estimate(XL, chr(vec), teach)
-    
-    vec[n]
+  computed = function(vec) {
+    if (is.na(vec[size + 1])) {
+      est = estimate(XL, chr(vec), teach)
+      vec[size + 1] = est$ext
+      vec[size + 2] = est$int
+    }
+    vec
   }
   
-  for(i in 1:maxPopulationSize) {
-    R[i, ] = c(sample(0:1, size, T), NA)
-    R[i, size + 1] = Q(R[i, ])
-  }
+  for(i in 1:maxPopulationSize)
+    R[i, ] = computed(c(sample(0:1, size, T, prob=c(1-startOnesProbab, startOnesProbab)), NA, NA))
   
   best = R[1, ]
   bestIteration = 0
@@ -63,38 +70,47 @@ geneticSelect = function(iterations,  # количество итераций
   plot(x=c(0, iterations), y=c(0,max(R[, size + 1])), col=c(0,0),
        xlab="Номер итерации", ylab="переобучение")
   
-  for(iter in 1:iterations) {
+  for (iter in 1:iterations) {
     print(c("iteration", iter))
     
     # sort
-    popSize = nrow(R)
-    for(i in 1:popSize)
-      R[i, size + 1] = Q(R[i,])
-    R = R[order(R[,size + 1]), ]
+    for (i in 1:nrow(R))
+      R[i, ] = computed(R[i, ])
+    R = R[order(R[, size + 1]), ]
     
-    if (popSize > maxPopulationSize) { # отбор лучших
-      popSize = maxPopulationSize;
-      R = R[1:popSize, ]
+    if (removeDuplicates) {
+      # удаление повторяющихся экземпляров (если ответы разные, то оставить лучшего)
+      R = R[!duplicated(R[, -ncol(R)]), ]
     }
     
-    if (Q(R[1, ]) < Q(best)) {
+    if (nrow(R) > maxPopulationSize) { # отбор лучших
+      R = R[1:maxPopulationSize, ]
+    }
+    
+    if (R[1, size + 1] < best[size + 1]) {
       best = R[1, ]
       bestIteration = iter
     }
     
+    popSize = nrow(R)
     points(x=rep(iter, popSize), y=R[,size + 1], pch=rep(16, popSize), col=rep("blue", popSize))
-    print(R[1, ])
+    print(
+      c(
+        R[1, ], 
+        paste0('(', sum(chr(R[1, ])), ')')
+      )
+    )
     
     if (iter - bestIteration >= d)
       break
     
-    for(i in 1:popSize) {
-      for(j in 1:(popSize - 1)) {
-        child = mutation(crossover(chr(R[i,]), chr(R[j,])), mutationProb)
-        R = rbind(R, c(child, NA))
+    for (i in 1:popSize) {
+      for (j in 1:i) {
+        child = mutation(crossover(chr(R[i, ]), chr(R[j, ])), mutationProb)
+        R = rbind(R, c(child, NA, NA))
       }
     }
   }
   
-  c(chr(best), Q(best))
+  best
 }
--- championship10/main.R
+++ championship10/main.R
@@ -31,8 +31,6 @@ extendCols = function (XX) {
   XX = XX[, which(1 == c(0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0,1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0,0, 0, 0, 0, 1, 0, 0,1, 1, 1, 0, 1, 1, 1,1, 1, 0, 1, 0, 0, 1,0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0))]
   XX = XX[, which(1 == c(1, 1, 1, 1, 0, 0, 1, 1, 1,1, 0, 0, 1, 0, 0, 1, 0, 1,1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1))]
   
-  #XX = XX[, c(130, 124, 132, 20,  32,  12,  133, 49,  101, 131, 75,  91,  43,  21,  62,  109, 135, 4,   139, 129, 119, 64, 100, 25,  81,  126, 137, 27,  105, 69,  23,  1,   121)]
-  
   XX
 }
 insertXLCol = function (XL, Z) {
@@ -53,12 +51,8 @@ preCols = function (XX) {
 
 XX = preCols(XX)
 
+XLL = as.matrix(unname(cbind(data.matrix(XX), YY)))
 
-"
-pc = princomp(XX)
-XX = XX %*% solve(t(pc$loadings))
-XX = XX[, 1:20]
-"
 
 my.extendedColsTrain = function (XL, trainFunc) {
   XL = extendXYCols(XL)
@@ -93,16 +87,15 @@ my.train.xgb = function (XLL, iters=10, rowsFactor=0.3, aggregator=meanAggregato
     XK = XLL[-sampleIdxes, ]
     XL = XLL[sampleIdxes, ]  
     
-    dtrain <- xgb.DMatrix(data=XL[, -ncol(XL)], label=XL[, ncol(XL)])
-    dtest <- xgb.DMatrix(data=XK[, -ncol(XK)], label=XK[, ncol(XK)])
-    watchlist <- list(train=dtrain, test=dtest)
+    dtrain = xgb.DMatrix(data=XL[, -ncol(XL)], label=XL[, ncol(XL)])
+    dtest = xgb.DMatrix(data=XK[, -ncol(XK)], label=XK[, ncol(XK)])
+    watchlist = list(train=dtrain, test=dtest)
     
-    algos[[it]] = local({
-      bstSparse <- xgb.train(data=dtrain, watchlist=watchlist, max_depth=3, gamma=5, eta=0.06, nthread=8, nrounds=3000, eval_metric='logloss', objective='binary:logistic', early_stopping_rounds=50, verbose=0)
-      function (X) {
-        predict(bstSparse, X)
-      }
-    })
+    algos[[it]] = xgb.train(
+      data=dtrain, watchlist=watchlist, max_depth=3, gamma=5, eta=0.06, 
+      nthread=8, nrounds=3000, eval_metric='logloss', objective='binary:logistic',
+      early_stopping_rounds=50, verbose=0
+    )
   }
   aggregator(algos)
 }
@@ -115,69 +108,41 @@ my.train.lgb = function (XLL, iters=10, rowsFactor=0.3, aggregator=meanAggregato
     XK = XLL[-sampleIdxes, ]
     XL = XLL[sampleIdxes, ]  
     
-    dtrain <- lgb.Dataset(data=XL[, -ncol(XL)], label=XL[, ncol(XL)], free_raw_data=FALSE)
-    dtest <- lgb.Dataset(data=XK[, -ncol(XK)], label=XK[, ncol(XK)], free_raw_data=FALSE)
-    valids <- list(train=dtrain, test=dtest)
+    dtrain = lgb.Dataset(data=XL[, -ncol(XL)], label=XL[, ncol(XL)], free_raw_data=FALSE)
+    dtest = lgb.Dataset(data=XK[, -ncol(XK)], label=XK[, ncol(XK)], free_raw_data=FALSE)
+    valids = list(train=dtrain, test=dtest)
     
-    algos[[it]] = local({
-      bst <- lgb.train(data=dtrain, num_leaves=7, max_depth=3, learning_rate=0.06, nrounds=1000, valids=valids,
-                       eval = c('binary_logloss'), objective = 'binary',
-                       nthread = 4, verbose=0, early_stopping_rounds=50,
-                       min_data_in_leaf=100, lambda_l2=5)
-      
-      function (X) {
-        predict(bst, X)
-      }
-    })
+    algos[[it]] = lgb.train(
+      data=dtrain, num_leaves=7, max_depth=3, learning_rate=0.06,
+      nrounds=1000, valids=valids, 
+      eval=c('binary_logloss'), objective = 'binary',
+      nthread=4, verbose=0, early_stopping_rounds=50,
+      min_data_in_leaf=100, lambda_l2=5
+    )
   }
   aggregator(algos)
 }
 
-XLL = as.matrix(unname(cbind(data.matrix(XX), YY)))
-
-#clr = ifelse(XLL[,ncol(XLL)]==0,"red","green")
-#pairs(XLL[,1:3], col=clr)
-
-
-"
-xxx = XLL[, -ncol(XLL),drop=F]
-aaa = XLL[, ncol(XLL)]
-aaa <- factor(aaa, labels=c('a', 'b'))
-
-xxx2 = XLL[10001:15000, -ncol(XLL),drop=F]
-aaa2 = XLL[10001:15000, ncol(XLL)]
-aaa2 <- factor(aaa2, labels=c('a', 'b'))
-
-trControl=trainControl(method='cv', number=5, classProbs=TRUE, summaryFunction=mnLogLoss)
-tuGrid = expand.grid(
-  nrounds = 2000, 
-  max_depth = c(2, 3, 4, 5), 
-  eta = 0.06, 
-  gamma = c(0.5, 1, 2, 3), 
-  colsample_bytree = c(0.9, 0.95, 1.0), 
-  min_child_weight = c(0.5, 1, 1.5, 2, 2.5, 3, 2.2, 0.2),
-  subsample = 1
-)
-model <- train(xxx, aaa, method='xgbTree', metric='logLoss', maximize=F, trControl=trControl, tuneGrid=tuGrid, verbose=T)
-
-pr = predict(model, xxx, type='prob')$b
-print( error.logloss(c(aaa)-1, pr) )
-
-pr2 = predict(model, xxx2, type='prob')$b
-print( error.logloss(c(aaa2)-1, pr2) )
-
-
-ctrl <- rfeControl(functions=caretFuncs, method='cv', repeats=5, verbose=F)
-colnames(xxx) <- paste('col', 1:12, sep='')
-lmProfile <- rfe(xxx, aaa, sizes=c(1:5, 10), rfeControl=ctrl, method='nnet')
-"
-
 mlpTeachAlgo = function (X, Y) {
-  Y = factor(Y, levels=c("0", "1"))
-  model = train(X, Y, method='mlp', maxit=30)
+  Y = factor(Y, labels=c('a', 'b'))
+  
+  trControl = trainControl(method='cv', number=5, repeats=1, classProbs=T, summaryFunction=mnLogLoss)
+  
+  tuneGrid = expand.grid(
+    size = 5:7
+  )
+  
+  capture.output(
+    model <- train(X, Y, method='mlp', metric='logLoss', maxit=2000, 
+                   maximize=F, trControl=trControl, verbose=F,
+                   tuneGrid=tuneGrid)
+  )
+  
+  mmm <<- model
+  print(model)
   
   function (X) {
-    predict(model, X, type='prob')$`1`
+    predict(model, X, type='prob')$b
   }
 }
 nnetTeachAlgo = function (X, Y) {
@@ -204,32 +169,6 @@ nnetTeachAlgo = function (X, Y) {
   }
 }
 
-teachAlgo = function (XL) {
-  X = XL[, -ncol(XL)]
-  Y = XL[, ncol(XL)]
-  mlpAlg = mlpTeachAlgo(X, Y)
-  XL = cbind(X, mlpAlg(X), Y)
-
-  model = my.teach(XL, rowsFactor=0.6, iters=15, colsFactor=1)
-  
-  function (X) {
-    X = cbind(X, mlpAlg(X))
-    model(X)
-  }
-}
-teachAlgo = function (XL) {
-  X = XL[, -ncol(XL)]
-  Y = XL[, ncol(XL)]
-  svmAlg = svmTeachAlgo(XL)
-  XL = cbind(X, svmAlg(X), Y)
-  
-  model = my.teach(XL, rowsFactor=0.6, iters=2, colsFactor=1)
-  
-  function (X) {
-    X = cbind(X, svmAlg(X))
-    model(X)
-  }
-}
 
 svmTrainAlgo = function (XL) {
   my.normalizedTrain(XL, function (XL) {
@@ -237,8 +176,16 @@ svmTrainAlgo = function (XL) {
     X = XL[, -ncol(XL)]
     Y = factor(XL[, ncol(XL)], labels=c('a', 'b'))
     
+    tuneGrid = expand.grid(weight=c(5, 10), cost=c(0.5, 1, 2, 5))
     trControl = trainControl(method='cv', number=3, classProbs=T, summaryFunction=mnLogLoss)
-    capture.output(model <- train(X, Y, method='svmRadial', metric='logLoss', maximize=F, trControl=trControl, verbose=F))
+    capture.output(
+      model <- train(X, Y, method='svmLinearWeights', metric='logLoss', maximize=F, 
+                     trControl=trControl, tuneGrid=tuneGrid,
+                     verbose=F)
+    )
+    
+    svmModel1 <<- model
+    print(model)
     
     function (X) {
       predict(model, X, type='prob')$b
@@ -249,6 +196,12 @@ svmTrainAlgo = function (XL) {
 nnetTrainAlgo = function (XL) {
   my.normalizedTrain(XL, function (XL) {
     nnetTeachAlgo(XL[, -ncol(XL)], XL[, ncol(XL)])
+  })
+}
+
+mlpTrainAlgo = function (XL) {
+  my.normalizedTrain(XL, function (XL) {
+    mlpTeachAlgo(XL[, -ncol(XL)], XL[, ncol(XL)])
   })
 }
 
@@ -275,16 +228,22 @@ lgbnNnetAggregatedTrain = function (XL) {
   ))
 }
 
-#print(validation.tqfold(XLL, lgbTrainAlgo, folds=5, iters=6, verbose=T))
-#print(geneticSelect(iterations=200, XL=XLL, teach=function (XL) {my.teach(XL, rowsFactor=0.6, iters=3, colsFactor=1)}, maxPopulationSize=15, mutationProb=0.2))\
+#print(validation.tqfold(XLL, svmTrainAlgo, folds=5, iters=6, verbose=T))
+#print(geneticSelect(iterations=200, XL=extendXYCols(XLL), teach=function (XL) {
+#  my.normalizedTrain(XL, function (XL) {
+#    my.train.lgb(XL, rowsFactor=0.9, iters=4)
+#  })
+#}, maxPopulationSize=13, mutationProb=0.2, startOnesProbab=0.2))
 #my.rfe(XLL)
 
 
-a3 = lgbTrainAlgo(XLL)
+#a3 = lgbTrainAlgo(XLL)
 #a2 = xgbTrainAlgo(XLL)
-a1 = nnetTrainAlgo(XLL)
+#a1 = nnetTrainAlgo(XLL)
+#a4 = svmTrainAlgo(XLL)
+#a5 = mlpTrainAlgo(XLL)
 "
-alg = meanAggregator(c(a1, a3))
+alg = meanAggregator(c(a4, a3))
 XXX = read.csv(file='x_test.csv', head=T, sep=';', na.strings='?')
 XXX = preCols(XXX)
 results = alg(XXX)
