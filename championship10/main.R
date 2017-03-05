610089371d452ad992c82053b18b36c66c1ba3f3
tyamgin <tyamgin@gmail.com>
2017-03-05 23:53:22 +0300

    .

--- championship10/main.R
+++ championship10/main.R
@@ -124,7 +124,7 @@ my.normalizedTrain = function (XL, trainFunc) {
 
 
 
-alg = meanAggregator(c(algb, annetbt, annetmagic))
+alg = meanAggregator(c(algb, algb, annetbt, annetmagic))
 XXX = read.csv(file='x_test.csv', head=T, sep=';', na.strings='?')
 XXX = preCols(XXX)
 results = alg(XXX)
