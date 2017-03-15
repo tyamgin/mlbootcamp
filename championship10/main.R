0166ef4b2ce8604e13d2c471439f98a5acef127d
tyamgin <tyamgin@gmail.com>
2017-03-16 00:01:31 +0300

    .

--- championship10/main.R
+++ championship10/main.R
@@ -151,7 +151,7 @@ my.fromFileAlgo = function (path) {
 #stopCluster(cl)
 
 print('computed')
-alg = gmeanAggregator(c(my.fromFileAlgo('algb200.txt'), my.fromFileAlgo('nnet200_09_20.txt')))
+alg = gmeanAggregator(c(my.fromFileAlgo('lgb500old_631b.txt'), my.fromFileAlgo('nnet200_09_20.txt')))
 #alg = logitTrainAlgo(XLL, c(my.fromFileAlgo('lgb500old_631b.txt'), my.fromFileAlgo('nnet200_08_10.txt')))
 #alg = annetbt2
 
