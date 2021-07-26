b9b71f67101dfde91fabbcf739c34f574e6f54a1
Ivan Tyamgin <tyamgin@yandex-team.ru>
2021-07-26 19:57:30 +0300

    .

--- vkcup2021_2/main.py
+++ vkcup2021_2/main.py
@@ -8,7 +8,8 @@ from lib import (
 
 def main():
     parser = argparse.ArgumentParser(description='Program.')
-    parser.add_argument('--data-dir', dest='data_dir', required=True)
+    parser.add_argument('--test-data-dir', dest='test_data_dir', required=True)
+    parser.add_argument('--train-data-dir', dest='train_data_dir', required=True)
     parser.add_argument('--work-dir', dest='work_dir', required=True)
     parser.add_argument('--res-path', dest='res_path', required=True)
     args = parser.parse_args()
@@ -26,10 +27,15 @@ def main():
         'bagging_freq': 5,
         'num_boost_round': 200,
     })
-    model.load(os.path.join(args.work_dir, 'models/lgb.txt'))
+    train = Data()
+    train.read(args.train_data_dir, 'train')
 
     test = Data()
-    test.read(args.data_dir, 'test')
+    test.read(args.test_data_dir, 'test')
+
+    model.prepare(train, test)
+
+    model.load(os.path.join(args.work_dir, 'models/lgb.txt'))
 
     res = model.predict(test)
     res.rename(columns={'res': 'age'}).to_csv(args.res_path, header=True, index=False)
