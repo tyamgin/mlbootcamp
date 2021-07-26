bbb24a555c79a553792c6d0344eb30ae0d8d59bf
Ivan Tyamgin <tyamgin@yandex-team.ru>
2021-07-26 19:49:08 +0300

    .

--- vkcup2021_2/lib.py
+++ vkcup2021_2/lib.py
@@ -12,8 +12,6 @@ class Data:
     friends = None
     edu = None
     groups = None
-    registered_year_by_uid = None
-    age_by_uid = None
 
     def _prepare_friends(self, csv):
         res = defaultdict(list)
@@ -35,32 +33,28 @@ class Data:
             data.set_index('uid')).reset_index()
         self.groups = self._prepare_groups(pd.read_csv(os.path.join(data_folder, f'{prefix}Groups.csv')))
 
-        is_test = 'age' not in self.edu.columns
-
-        self.registered_year_by_uid = {}
-        if not is_test:
-            self.age_by_uid = {}
-        for row in self.edu.itertuples():
-            self.registered_year_by_uid[row.uid] = row.registered_year
-            if not is_test:
-                self.age_by_uid[row.uid] = row.age
-
     def get(self, idxes):
         result = Data()
         result.friends = self.friends
         result.edu = self.edu[self.edu['uid'].isin(idxes)]
         result.groups = self.groups  # TODO
-        uids_set = set(idxes)
-        result.registered_year_by_uid = {uid: v for uid, v in self.registered_year_by_uid.items() if uid in uids_set}
-        result.age_by_uid = {uid: v for uid, v in self.age_by_uid.items() if uid in uids_set}
         return result
 
 
 class MyModel:
     verbose = 0
+    registered_year_by_uid = None
+    age_by_uid = None
 
-    def __init__(self, params=None):
+    def __init__(self, params):
         self.params = params or {}
+
+    def prepare(self, train, test):
+        self.registered_year_by_uid = {}
+        self.age_by_uid = {}
+        for row in train.edu.itertuples():
+            self.registered_year_by_uid[row.uid] = row.registered_year
+            self.age_by_uid[row.uid] = row.age
 
     def get_X(self, data):
         del_cols = ['age']
@@ -70,31 +64,46 @@ class MyModel:
         res['groups_count'] = [len(data.groups.get(uid, [])) for uid in uids]
         res['friends_median_registered_year'] = [
             np.median([
-                data.registered_year_by_uid[fr]
+                self.registered_year_by_uid[fr]
                 for fr in data.friends.get(uid, [])
-                if fr in data.registered_year_by_uid
+                if fr in self.registered_year_by_uid
             ])
             for uid in uids
         ]
         res['friends_median_age'] = [
             np.median([
-                data.age_by_uid[fr]
+                self.age_by_uid[fr]
                 for fr in data.friends.get(uid, [])
-                if fr in data.age_by_uid
+                if fr in self.age_by_uid
             ])
             for uid in uids
         ]
 
         return res
 
-    def fit(self, data):
+    def fit(self, X):
         pass
 
     def predict(self, edu):
         return np.repeat(35, edu.shape[0])
 
+dummy_data = Data()
+dummy_data.edu = pd.DataFrame({
+    'uid': [356],
+    'school_education': [2010],
+    'graduation_1': [2010],
+    'graduation_2': [2010],
+    'graduation_3': [2010],
+    'graduation_4': [2010],
+    'graduation_5': [2010],
+    'graduation_6': [2010],
+    'graduation_7': [2010],
+    'age': [27],
+    'registered_year': [2015],
+})
 dummy_model = MyModel(None)
-
+dummy_model.prepare(dummy_data, None)
+dummy_model.fit(dummy_data)
 
 class LgbModel(MyModel):
     model = None
